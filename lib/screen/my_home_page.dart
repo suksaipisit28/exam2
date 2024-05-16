05f81d9585a055e51adb850e11352fb206245ca3
pisit suksai <pisit_suksai@hotmail.com>
2024-05-16 16:59:10 +0700

    success1

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--;
+      } else {
+        _counter = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My increment couter'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("ค่า $_counter"),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Icon(Icons.add),
+            ),
+            ElevatedButton(
+              onPressed: _decrementCounter,
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
